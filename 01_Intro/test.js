7843fe9a0f78254a17b273bca744c401761efebd
Ramkumar-1994 <nramkumar1994@gmail.com>
2025-02-05 10:39:13 +0530

    intro_typescript

---
+++ 01_Intro/test.js
@@ -0,0 +1,23 @@
+var a=12;
+var b=23;
+var a=24;    //Not good so typescript is used
+
+var a=32;
+a="Harsh";     //a is declared with value number and reassigned to a string.
+
+//Typescript is used for type safety and few new features.Typescript is javascript with some improved version.
+
+// Installing Typescript
+
+// npm i typescript -g
+
+// To run typescript file tsc filename eg:app.ts then app.js file will be created
+
+// Extension required
+
+// ESLint
+// Javascript and typescript Nightly
+// Live server
+// Prettier
+// Prettier Eslint
+// Pretty Typescript Errors
